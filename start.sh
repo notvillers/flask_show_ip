c3e3b9445302ac554d21c29bad11980b5d6614d5
Krisztián Villers <krisztian@villers.hu>
2025-03-19 12:47:24 +0100

    renamed wsgi to be more visible in 'htop'

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ port=1338
 script_dir=$(dirname "$0")
 cd $script_dir
 source .venv/bin/activate
-mod_wsgi-express start-server wsgi.py --processes 2 --port $port
+mod_wsgi-express start-server flask_ip_wsgi.py --processes 1 --port $port
 deactivate
